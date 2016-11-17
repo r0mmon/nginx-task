4c62154a0a073204ad9da7a9872a091a541c8e8c
r0mmon <rommonmail@gmail.com>
2016-11-17 16:42:29 +0300

    Решенное задание: WSGI приложение должно возвращать документ с MIME-типом text/plain, содержащий все GET параметры, по одному на каждую строку.

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+from cgi import parse_qs, escape
+
 def wsgi_application(environ, start_response):
-# бизнес-логика
     status = '200 OK'
     headers = [
     ('Content-Type', 'text/plain')
     ]
-    body = 'Hello, world!'
+    d = parse_qs(environ['QUERY_STRING'])
+    body = ''
+    for key, value in d.items():
+        print(key, value[0])
+        body += key + '=' + value[0] +'\n'
     start_response(status, headers )
     return [ body ]
