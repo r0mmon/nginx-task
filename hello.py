492d726ab034762dfce885dfeafa0a438c117833
r0mmon <rommonmail@gmail.com>
2016-11-17 18:47:52 +0300

    Локально работает

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
-from cgi import parse_qs, escape
-
+import string
 def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [
     ('Content-Type', 'text/plain')
     ]
-    d = parse_qs(environ['QUERY_STRING'])
-    body = ''
-    for key, value in d.items():
-        body += key + '=' + value[0] +'\n'
+    str = (environ['QUERY_STRING'])
+    body = string.replace(str, '&', '\n')
     start_response(status, headers )
     return [ body ]
